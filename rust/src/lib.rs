9d856af63a2da3cf8f40f9dd91514b5701079a85
Jochen Görtler <grtlr@users.noreply.github.com>
2022-05-27 13:36:14 +0200

    Better `MilestoneRequests` (#57)

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -26,3 +26,21 @@ pub use self::proto::{inx_client as client, inx_server as server};
 
 #[cfg(feature = "types")]
 pub use self::types::*;
+
+impl proto::MilestoneRequest {
+    /// Creates a [`MilestoneRequest`](proto::MilestoneRequest) from an milestone index.
+    pub fn from_index(milestone_index: u32) -> Self {
+        Self {
+            milestone_index,
+            milestone_id: None,
+        }
+    }
+
+    /// Creates a [`MilestoneRequest`](proto::MilestoneRequest) from a [`MilestoneId`](proto::MilestoneId).
+    pub fn from_id(milestone_id: proto::MilestoneId) -> Self {
+        Self {
+            milestone_index: 0,
+            milestone_id: Some(milestone_id),
+        }
+    }
+}
--- rust/src/types/milestone/mod.rs
+++ rust/src/types/milestone/mod.rs
@@ -2,7 +2,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
 mod range;
-mod request;
 
 use super::Error;
 use crate::proto;
