3db907e1dbe993afe55820db3ff713d90d348373
Jochen Görtler <jochen.goertler@iota.org>
2022-07-15 12:32:00 +0200

    Separate Rust `inx` and `bee-inx`

--- rust/inx/src/lib.rs
+++ rust/inx/src/lib.rs
@@ -5,9 +5,6 @@
 
 //! This crate provides bindings for the IOTA node extensions (INX).
 
-#[cfg(feature = "types")]
-mod types;
-
 /// The raw protobuf definitions.
 pub mod proto {
     #![allow(missing_docs)]
@@ -23,9 +20,6 @@ pub mod tonic {
 }
 
 pub use self::proto::{inx_client as client, inx_server as server};
-
-#[cfg(feature = "types")]
-pub use self::types::*;
 
 impl proto::MilestoneRequest {
     /// Creates a [`MilestoneRequest`](proto::MilestoneRequest) from an milestone index.
