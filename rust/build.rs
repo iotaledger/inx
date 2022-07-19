a52f269380bf76eb47f32a6903d02cbf920002b0
Jochen Görtler <jochen.goertler@iota.org>
2022-07-19 12:22:39 +0200

    Enable vendoring

--- rust/build.rs
+++ rust/build.rs
@@ -1,6 +1,15 @@
 // Copyright 2020-2022 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
+use std::env;
+
 fn main() -> Result<(), std::io::Error> {
-    tonic_build::compile_protos("../proto/inx.proto")
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    std::fs::copy(
+        format!("{manifest_dir}/../proto/inx.proto"),
+        format!("{out_dir}/inx.proto"),
+    )?;
+    tonic_build::compile_protos(format!("{out_dir}/inx.proto"))?;
+    Ok(())
 }
